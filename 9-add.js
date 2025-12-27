9cff6dee56ba9cad950bf422e3855eb322cd8e26
raelweb <israelkyesutor@gmail.com>
2025-12-27 00:13:44 +0000

    Write a script that prints the addition of 2 integers.

---
+++ 9-add.js
@@ -0,0 +1,13 @@
+function addInt(a,b){
+    return a + b;
+}
+console.log(addInt(10,12));
+/*
+function addInt(a,b){
+    return a + b;
+}
+    const num1 = 10;
+    const num 2 = 12;
+
+console.log(addInt(num1,num2));
+*/
