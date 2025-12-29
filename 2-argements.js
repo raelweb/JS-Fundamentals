392c312fe0fbeee5601f8d42a4226e9596353692
raelweb <israelkyesutor@gmail.com>
2025-12-29 17:17:37 +0000

    Print message depending on number of arguments

--- 2-argements.js
+++ 2-argements.js
@@ -1,10 +1,9 @@
-const argCount = process.argv.length -2;
-if(argCount === 0){
-    console.log ("No argument");
-} else if(argCount === 1){
-    console.log ("Argument found");
-} else{
-    console.log ("Arguments found");
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-// prints number of arguments
-console.log(argCount)
