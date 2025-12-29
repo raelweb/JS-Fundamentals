03f87e7986b8f8cb39dbe17a3f28ac497d300489
raelweb <israelkyesutor@gmail.com>
2025-12-29 16:32:44 +0000

    fix script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,8 @@
-const arg = process.argv[2]
-const number = parseInt(arg)
-if (number.isNan(number)){
-    comsole.log("Not a number");
-}else{
-    console.log("My number: " + number);
+const arg = process.argv[2];
+const number = parseInt(arg);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
 }
