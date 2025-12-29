ed6b36c03099a30621ebd5fdff86b292e619a729
raelweb <israelkyesutor@gmail.com>
2025-12-29 16:35:29 +0000

    rint two arguments in the format '<arg1> is <arg2>

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,6 @@
-const firstArg = Process.argv[2];
-const secondArg = process.argv[3]
-console.log(firstArg + " is " + secondArg);
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + ' is ' + arg2);
